f2dfcc5268cf429717d2c63451fa5a15f5e8bce4
sergiobn <sergiobn@users.noreply.github.com>
2023-04-03 10:10:26 +0000

    added post model
    added Create Post route

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, jsonify, request
-from Models.posts import posts
+from Models.posts import posts,Post
 app = Flask(__name__)
 
 
@@ -12,6 +12,13 @@ def main():
 @app.route('/posts', methods=['GET'])
 def hello():
     return jsonify({"posts": posts})
+
+@app.route('/post', methods=['POST', 'GET'])
+def add_post():
+    post = Post(request.json["title"], request.json["description"])
+    posts.append(post)
+    return jsonify(post.to_json()), 201
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
