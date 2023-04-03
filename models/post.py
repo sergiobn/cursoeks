159aae8cb8aaea17eef702099c2ec6c57070872a
sergiobn <sergiobn@users.noreply.github.com>
2023-04-03 10:10:26 +0000

    added post model
    added Create Post route

--- models/post.py
+++ models/post.py
@@ -11,3 +11,16 @@ posts = {
         "Description": "I love this city too!"
         }
 }
+
+class Post():
+    def __init__(self, title, content):
+        self.title = title
+        self.description = content
+    def to_json(self):
+        return {
+            "title": self.title,
+            "description": self.description
+        }
+    def from_json(self, json):
+        self.title = json["title"]
+        self.description = json["description"]
