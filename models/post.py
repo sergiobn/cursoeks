53e812ad42d2505aa6878a1af031488b5266a6ed
sergiobn <sergiobn@users.noreply.github.com>
2023-04-03 10:02:57 +0000

    added code base to Develop branch

---
+++ models/post.py
@@ -0,0 +1,13 @@
+posts = {
+    1: {
+        "title": "Hello World!",
+        "Description": "This is my first post!"
+    },
+    2: {
+        "title": "San Francisco",
+        "Description": "I love this city!"
+    },
+    3: {"title": "New York",
+        "Description": "I love this city too!"
+        }
+}
